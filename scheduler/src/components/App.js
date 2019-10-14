e5f1e30cba488757af3b53091cf07d3f82982afb
ZoeDj <studio3fun@gmail.com>
2019-10-13 21:24:12 -0400

    fetch, state, props

--- scheduler/src/components/App.js
+++ scheduler/src/components/App.js
@@ -8,19 +8,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      myName: "Zoe"
+      myAppointments: [],
+      lastIndex: 0
     };
   }
+
+  componentDidMount() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
   render() {
     return (
       <main className="page bg-white" id="dentistry">
         <div className="container">
           <div className="col-md-12 bg-white">
             <div className="container">
-              {this.state.myName}
               <AddAppointments />
               <SearchAppointments />
-              <ListAppointments />
+              <ListAppointments appointments={this.state.myAppointments} />
             </div>
           </div>
         </div>
--- scheduler/src/components/ListAppointments.js
+++ scheduler/src/components/ListAppointments.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
+import { FaTimes } from "react-icons/fa";
+import Moment from "react-moment";
 
 class ListAppointments extends Component {
   render() {
-    return <div>List Appointments</div>;
+    return (
+      <div className="appointment-list item-list mb-3">
+        {this.props.appointments.map(item => (
+          <div className="pet-item col media py-3 key={ item.aptId }">
+            <div className="mr-3">
+              <button className="pet-delete btn btn-sm btn-danger">
+                <FaTimes />
+              </button>
+            </div>
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span className="pet-name">{item.petName}</span>
+                <span className="app-date ml-auto">
+                  <Moment
+                    date={item.aptDate}
+                    parse="YYYY-MM-dd hh:mm"
+                    format="MMM-D, h:mma"
+                  />
+                </span>
+              </div>
+              <div clasName="owner-name">
+                <span className="label-item">{item.ownerName}</span>
+                <span>ownerName</span>
+              </div>
+              <div className="apt-notes">{item.aptNotes}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 export default ListAppointments;
